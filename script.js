a812382e79052e40c42baab3e0490211b7eeb4aa
Tarus <taruspande02@gmail.com>
2023-11-09 17:45:42 +0530

    finished

--- script.js
+++ script.js
@@ -8,3 +8,55 @@ function changeImage(index) {
     images[index].classList.add('active');
     buttons[index].classList.add('selected');
 }
+
+function selectQuantity(index){
+    const buttons = document.querySelectorAll('.quantity-item');
+    buttons.forEach(button=>button.classList.remove('selected'));
+    buttons[index].classList.add('selected');
+}
+
+function purchaseMethod(index){
+    const buttons = document.querySelectorAll('.purchase-btn');
+    const frequencyBtn = document.querySelectorAll('.frequency');
+    buttons.forEach(button=>button.classList.remove('selected'));
+    buttons[index].classList.add('selected');
+    if(index===1){
+        frequencyBtn.forEach(freq=>freq.classList.remove('hidden'));
+    }
+    else{
+        frequencyBtn.forEach(freq=>freq.classList.add('hidden'));
+    }
+}
+
+function openDropDown(){
+    const options = document.querySelectorAll('.frequency-item');
+    options.forEach(option=>option.classList.add('visible'));
+}
+
+function selectOption(index){
+    const options = document.querySelectorAll('.frequency-item');
+    options.forEach(option=>option.classList.remove('visible'));
+    console.log(options);
+    options[index].classList.add('visible');
+    console.log(options);
+}
+
+function selectDesc(index){
+    const buttons = document.querySelectorAll('.desc-toggle');
+    const texts = document.querySelectorAll('p.description-text');
+    buttons.forEach(button=>button.classList.remove('selected'));
+    texts.forEach(text=>text.classList.remove('visible'));
+    buttons[index].classList.add('selected');
+    if(index===0){
+        const queries = document.querySelectorAll('.desc-text');
+        queries.forEach(query=>query.classList.add('visible'));
+    }
+    else if(index===1){
+        const queries = document.querySelectorAll('.harvest-text');
+        queries.forEach(query=>query.classList.add('visible'));
+    }
+    else{
+        const queries = document.querySelectorAll('.use-text');
+        queries.forEach(query=>query.classList.add('visible'));
+    }
+}
